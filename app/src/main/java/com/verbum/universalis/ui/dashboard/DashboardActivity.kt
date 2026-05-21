1fac9ff79fe75ee82705d0ebdf4424a16bd7d87a
digitalhydra <jimnoname@gmail.com>
2026-05-21 15:47:18 -0500

    add scplash screen

--- app/src/main/java/com/verbum/universalis/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/verbum/universalis/ui/dashboard/DashboardActivity.kt
@@ -3,6 +3,7 @@ package com.verbum.universalis.ui.dashboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.verbum.universalis.core.theme.ThemeManager
 import com.verbum.universalis.core.theme.VerbumTheme
 import com.verbum.universalis.ui.navigation.MainScreen
@@ -11,6 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class DashboardActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         ThemeManager.initialize(this)
         setContent {
